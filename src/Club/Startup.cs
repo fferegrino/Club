a405f9216ba14028de6894c9a613a70177bda126
Antonio Feregrino <antonio.feregrino@gmail.com>
2015-12-23 05:52:07 -0600

    bower

--- src/Club/Startup.cs
+++ src/Club/Startup.cs
@@ -20,11 +20,8 @@ namespace Club
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
 
         // Entry point for the application.
