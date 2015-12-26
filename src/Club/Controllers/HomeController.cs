cb58eab4c147bd679e08fae01264a5e1173ec9c3
Antonio Feregrino <antonio.feregrino@gmail.com>
2015-12-26 10:08:26 -0600

    Lots of stuff going on rn

--- src/Club/Controllers/HomeController.cs
+++ src/Club/Controllers/HomeController.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Club.Models.ViewModels;
+using Club.Services;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,7 +9,13 @@ namespace Club.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
+        IMailService _mailService;
+
+        public HomeController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -18,6 +23,27 @@ namespace Club.Controllers
 
         public IActionResult Contact()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            var email = Startup.Configuration["AppSettings:SiteEmailAddress"];
+            if (String.IsNullOrWhiteSpace(email))
+            {
+                ModelState.AddModelError("", "ConfigurationError");
+            }
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(email, email, "Email", model.Message);
+                ViewBag.Message = "Sent";
+                ModelState.Clear();
+            }
+            //else
+            //{
+
+            //}
             return View();
         }
     }
---
+++ src/Club/Models/ViewModels/ContactViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Club.Models.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [StringLength(200,MinimumLength =5)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
