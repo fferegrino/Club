3b75ea64643123b169ee9877354bd648a3eec4a6
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-01-15 14:29:54 -0600

    Angular paging beginning

---
+++ src/Club/wwwroot/js/controllers/unapprovedUsersController.js
@@ -0,0 +1,30 @@
+﻿// unapprovedUsersController.js
+(function () {
+	"use strict";
+	angular.module("app-unapproved-users")
+        .controller("unapprovedUsersController", unapprovedUsersController);
+
+	function unapprovedUsersController($http) {
+		var vm = this;
+
+		vm.queriedUsers = [];
+
+		vm.currentPage = 1;
+		vm.pageSize = 5;
+	    vm.pageNumber = 1;
+
+		vm.load = function fetchNewData() {
+			var url = "/api/users/unapproved?pageSize=" + vm.pageSize + "&pageNumber=" + vm.pageNumber;
+			$http.get(url)
+                .then(function (response) {
+                    angular.copy(response.data.items, vm.queriedUsers);
+                },
+                function () {
+
+                });
+		}
+
+	    //fetchNewData(currentPage);
+	}
+
+})();
